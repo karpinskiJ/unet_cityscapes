41fe0f22b68ec107ab13b589dd3dcacda789b254
jkarpins <01150169@pw.edu.pl>
2024-04-02 21:31:31 +0200

    Setup added

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='unet_cityscapes',
+    version='0.0.0',
+    description='U-Net for Cityscapes dataset',
+    author='Jakub Karpiński & Andrzej Sawicki',
+    packages=setuptools.find_packages(),
+    zip_safe=False,
+)
