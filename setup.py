a2b6c13f6af8dfb40fd86615a01ad1fdda7559f6
karpinskiJ <01150169@pw.edu.pl>
2024-04-22 00:02:44 +0200

    Dropout added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='unet_cityscapes',
-    version='0.2.0',
+    version='0.3.0',
     description='U-Net for Cityscapes dataset',
     author='Jakub Karpiński',
     packages=setuptools.find_packages(),
