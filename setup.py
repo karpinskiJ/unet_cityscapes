f2184a1544654b47dd5cddce709fb120352f18a5
karpinskiJ <01150169@pw.edu.pl>
2024-04-24 22:53:31 +0200

    Batch norm added and FocalLoss added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='unet_cityscapes',
-    version='0.4.0',
+    version='0.5.0',
     description='U-Net for Cityscapes dataset',
     author='Jakub Karpiński',
     packages=setuptools.find_packages(),
